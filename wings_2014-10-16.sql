b28487b46c6e04efdcc314f8973e0912367e7320
Vicky <hardcoder77@gmail.com>
2014-10-16 00:57:14 +0530

    added sql file for db migration

---
+++ wings_2014-10-16.sql
@@ -0,0 +1,115 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4096
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: 127.0.0.1 (MySQL 5.6.21)
+# Database: wings
+# Generation Time: 2014-10-15 19:25:38 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table auth
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `auth`;
+
+CREATE TABLE `auth` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL DEFAULT '',
+  `password` varchar(64) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `auth` WRITE;
+/*!40000 ALTER TABLE `auth` DISABLE KEYS */;
+
+INSERT INTO `auth` (`id`, `username`, `password`)
+VALUES
+	(1,'vicky','YXNkZjEyMzQ=');
+
+/*!40000 ALTER TABLE `auth` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table product
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `product`;
+
+CREATE TABLE `product` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  KEY `name_index` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+
+INSERT INTO `product` (`id`, `name`)
+VALUES
+	(38,'moto-e'),
+	(39,'moto-f'),
+	(40,'moto-g'),
+	(41,'moto-h'),
+	(42,'moto-i'),
+	(35,'nokia-z');
+
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table product_attributes_values
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `product_attributes_values`;
+
+CREATE TABLE `product_attributes_values` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `pid` int(11) unsigned DEFAULT NULL,
+  `name` varchar(64) DEFAULT NULL,
+  `value` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `attribute` (`name`),
+  KEY `value` (`value`),
+  KEY `eav` (`pid`),
+  CONSTRAINT `eav` FOREIGN KEY (`pid`) REFERENCES `product` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `product_attributes_values` WRITE;
+/*!40000 ALTER TABLE `product_attributes_values` DISABLE KEYS */;
+
+INSERT INTO `product_attributes_values` (`id`, `pid`, `name`, `value`)
+VALUES
+	(8,35,'category','mobiles'),
+	(11,38,'category','mobiles'),
+	(12,39,'category','mobiles'),
+	(13,40,'category','mobiles'),
+	(14,41,'category','mobiles'),
+	(15,42,'category','mobiles');
+
+/*!40000 ALTER TABLE `product_attributes_values` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
